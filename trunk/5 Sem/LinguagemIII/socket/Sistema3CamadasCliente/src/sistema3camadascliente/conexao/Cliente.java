82be0954077ef11ed9085e262bc8de97878d56bc
manchini125 <manchini125@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-03-15 02:07:21 +0000

    Socket

--- trunk/5 Sem/LinguagemIII/socket/Sistema3CamadasCliente/src/sistema3camadascliente/conexao/Cliente.java
+++ trunk/5 Sem/LinguagemIII/socket/Sistema3CamadasCliente/src/sistema3camadascliente/conexao/Cliente.java
@@ -5,14 +5,10 @@
 package sistema3camadascliente.conexao;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import sistema3camadasbase.conexao.Mensagem;
 import sistema3camadasbase.conexao.Montador;
 
