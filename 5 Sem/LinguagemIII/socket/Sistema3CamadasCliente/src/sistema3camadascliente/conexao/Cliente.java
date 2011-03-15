a737eb5dfc085e231b3721b1e6a108d6d03319a9
manchini125 <manchini125@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-03-15 01:25:40 +0000

    Socket

---
+++ 5 Sem/LinguagemIII/socket/Sistema3CamadasCliente/src/sistema3camadascliente/conexao/Cliente.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema3camadascliente.conexao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sistema3camadasbase.conexao.Mensagem;
+import sistema3camadasbase.conexao.Montador;
+
+/**
+ *
+ * @author manchini
+ */
+public class Cliente {
+
+    public static String comando(final int tipo, final Object obj) throws Exception {
+        try {
+
+            Socket echoSocket = new Socket("localhost", 4445);
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            String retorno = "";
+            String readLine;
+            Thread.sleep(1000);
+
+            switch (tipo) {
+                case Mensagem.TIPO_INCLUIR:
+                    out.println(Mensagem.TIPO_INCLUIR + "&" + obj.toString());
+                    readLine = reader.readLine();
+                    retorno = readLine.substring(readLine.indexOf("&") + 1);
+                    break;
+                case Mensagem.TIPO_LISTAR:
+                    out.println(Mensagem.TIPO_LISTAR + "&" + obj.toString());
+                    readLine = reader.readLine();
+                    retorno = readLine.substring(readLine.indexOf("&") + 1);
+                    break;
+            }
+
+
+            return retorno;
+
+
+
+
+
+
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public static ArrayList toArrayList(String lista){
+   
+        ArrayList arrayList = new ArrayList();
+        String[] split = lista.split(";");
+        for (String string : split) {
+              arrayList.add(Montador.Montador(string));
+        }
+
+
+        return arrayList;
+    }
+
+}
