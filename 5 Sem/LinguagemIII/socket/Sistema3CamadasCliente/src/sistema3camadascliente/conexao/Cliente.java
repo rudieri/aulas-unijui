516a2cd4fe36b5f169b86e12567867f5f759030e
manchini125 <manchini125@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-03-22 23:09:55 +0000

    Arrumado pra Serialzable Servidor

--- 5 Sem/LinguagemIII/socket/Sistema3CamadasCliente/src/sistema3camadascliente/conexao/Cliente.java
+++ 5 Sem/LinguagemIII/socket/Sistema3CamadasCliente/src/sistema3camadascliente/conexao/Cliente.java
@@ -8,7 +8,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
-import java.util.ArrayList;
 import sistema3camadasbase.conexao.Mensagem;
 
 /**
