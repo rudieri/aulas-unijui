8e18175e25ab930df7322527055352563651c099
manchini125 <manchini125@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-03-22 01:24:50 +0000

    Socket Arrumado o Listar e Adicionado o Artista Adicionado o Resto das Classes

--- trunk/5 Sem/LinguagemIII/socket/Sistema3CamadasServidor/src/sistema3camadasservidor/GeraTabelasnoBanco.java
+++ trunk/5 Sem/LinguagemIII/socket/Sistema3CamadasServidor/src/sistema3camadasservidor/GeraTabelasnoBanco.java
@@ -3,6 +3,7 @@ package sistema3camadasservidor;
 
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import sistema3camadasservidor.banco.HibernateUtil;
+import sistema3camadasservidor.conexao.Servidor;
 
 public class GeraTabelasnoBanco {
 
@@ -12,9 +13,11 @@ public class GeraTabelasnoBanco {
     }
 
     public static void gerar() {
+        Servidor.startBanco();
         org.apache.log4j.BasicConfigurator.configure();
         SchemaExport se = new SchemaExport(HibernateUtil.getCfg());
         se.create(true, true);
+        System.exit(0);
 
     }
 }
