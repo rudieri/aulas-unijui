56d00758871aa81257f5c2ec10f8705f02b39d4d
AraraRTC <ararartc@gmail.com>
2011-03-28 15:09:18 +0000

    Nova interface grafica... JMusicaNova

---
+++ 5 Sem/LinguagemIII/socket/Sistema3CamadasCliente/test/MyListModel.java
@@ -0,0 +1,27 @@
+
+import javax.swing.AbstractListModel;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author rudieri
+ */
+public class MyListModel extends AbstractListModel {
+
+    private   String[] strings = {"lol"};
+
+    public void setElements(String[] s) {
+        strings = s;
+    }
+
+    public int getSize() {
+        return strings.length;
+    }
+
+    public Object getElementAt(int index) {
+        return strings[index];
+    }
+}
