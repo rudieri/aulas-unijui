97cbe358eba82ca386463a210e0737279267395d
ararartc@gmail.com <ararartc@gmail.com@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-06-04 16:55:40 +0000

    Capa adicionada.

--- trunk/5 Sem/Complexidade/jogoVelha/src/jogovelha/Loader.java
+++ trunk/5 Sem/Complexidade/jogoVelha/src/jogovelha/Loader.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jogovelha.ai.Jogador2;
+import jogovelha.interfaces.Jogador;
 import jogovelha.tabuleiro.Tabuleiro;
 import jogovelha.tela.TelaVelha;
 
@@ -17,24 +18,32 @@ import jogovelha.tela.TelaVelha;
  */
 public class Loader {
 
-    private Jogador2 computador;
+    private Jogador computador;
     private Tabuleiro tabuleiro;
     private TelaVelha telaVelha;
+    private final String jogadorPadrao="jogovelha.ai.Jogador2";
 
     public Loader() {
         try {
             telaVelha = new TelaVelha(null, true);
-            computador = new Jogador2();
+            computador = (Jogador) Class.forName(jogadorPadrao).newInstance();
             tabuleiro = new Tabuleiro(telaVelha, computador);
             computador.setTabuleiro(tabuleiro);
             telaVelha.setTabuleiroReal(tabuleiro);
             telaVelha.setVisible(true);
 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(IllegalAccessException ex){
+            
+        }catch(InstantiationException ex){
+            
         }
     }
 
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public static void main(String[] args) {
         new Loader();
     }
