f56b32dae5193d72454ac48d5ff3c62fb6edbc7e
fernandomanchini <fernandomanchini@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-08-18 22:29:45 +0000

    Tio Pac Desenho quase

---
+++ trunk/6 Sem/Computacao Grafica/Tio-Pac/src/br/com/manchini/tiopac/IndoPara.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.manchini.tiopac;
+
+/**
+ *
+ * @author manchini
+ */
+public enum  IndoPara {
+    CIMA,BAIXO,ESQUERDA,DIREITA
+}
