2808833faef30dfd704221ccbe8e2b50b1722ec3
fernandomanchini <fernandomanchini@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2011-08-18 22:29:45 +0000

    Tio Pac Desenho quase

--- 6 Sem/Computacao Grafica/Tio-Pac/src/br/com/manchini/tiopac/TioPacman.java
+++ 6 Sem/Computacao Grafica/Tio-Pac/src/br/com/manchini/tiopac/TioPacman.java
@@ -6,6 +6,7 @@ package br.com.manchini.tiopac;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Date;
 import javax.swing.JPanel;
 
 /**
@@ -15,35 +16,38 @@ import javax.swing.JPanel;
 public class TioPacman extends JPanel {
 
     boolean fechado = true;
+    IndoPara indoPara = IndoPara.DIREITA;
 
     @Override
     public void paint(Graphics grphcs) {
         super.paint(grphcs);
         paintS(grphcs);
     }
-    
-    
-  
-    public synchronized  void paintS(Graphics grphcs) {
+
+    public synchronized void paintS(Graphics grphcs) {
         grphcs.setColor(Color.black);
 //        grphcs.clearRect(0, 0, this.getWidth(), this.getHeight());
+        fechado = new Date().getTime() / 500 % 2 == 0;
         if (fechado) {
 //            desenarAberto(grphcs,Color.black);
-            desenarFechado(grphcs,Color.yellow);
+            desenarFechado(grphcs, Color.yellow);
         } else {
 //             desenarFechado(grphcs,Color.black);
-            desenarAberto(grphcs,Color.yellow);
+            desenarAberto(grphcs, Color.yellow);
         }
-        fechado= !fechado;
     }
 
-    private void desenarAberto(Graphics g,Color cor) {
+    private void desenarAberto(Graphics g, Color cor) {
         g.setColor(cor);
-        g.fillArc(0, 0, 50, 50, 30, 300);
+        if (indoPara == IndoPara.ESQUERDA) {
+            g.fillArc(0, 0, 50, 50, 30, 300);
+        } else if (indoPara == IndoPara.DIREITA) {
+            g.fillArc(0, 0, 50, 50, 200, 300);
+        }
     }
 
-    private void desenarFechado(Graphics g,Color cor) {
-       g.setColor(cor);
-        g.fillArc(0, 0, 50, 50, 5, 355);
+    private void desenarFechado(Graphics g, Color cor) {
+        g.setColor(cor);
+        g.fillArc(0, 0, 50, 50, 0, 360);
     }
 }
