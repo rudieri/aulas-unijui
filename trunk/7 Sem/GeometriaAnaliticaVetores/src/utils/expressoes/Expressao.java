05cf9683171477f25d3446ffcb38e75fe423209d
AraraRTC@gmail.com <AraraRTC@gmail.com@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2012-04-15 02:49:19 +0000

    Algumas classes uteis (Geometria Analitica e Vetores). Outtras coisas inúteis também... ;)

---
+++ trunk/7 Sem/GeometriaAnaliticaVetores/src/utils/expressoes/Expressao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils.expressoes;
+
+/**
+ *
+ * @author rudieri
+ */
+public class Expressao {
+    
+}
---
+++ trunk/7 Sem/GeometriaAnaliticaVetores/src/utils/expressoes/Variavel.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils.expressoes;
+
+/**
+ *
+ * @author rudieri
+ */
+public class Variavel {
+    private char nome;
+    private double valor;
+    private boolean inicializada;
+
+    public Variavel(char nome) {
+        this.nome = nome;
+        this.inicializada = false;
+    }
+    public Variavel(char nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+        this.inicializada = true;
+    }
+    
+    public boolean estaInicializada(){
+        return inicializada;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+        inicializada = true;
+    }
+
+    public char getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        if (inicializada) {
+            return valor;
+        }else{
+            throw new IllegalStateException("Variavel não contém um valor válido.");
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
