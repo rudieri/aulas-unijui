8ba9219275667b5efb9ca14415c46bf0b2b5a2d8
AraraRTC <ararartc@gmail.com>
2012-09-02 17:26:55 +0000

    Começando a fazer algo, mas com muita pouca vontade :(

--- 8 Sem/hci/HCIGerardeProva/src/java/br/hci/geradorprova/cadastro/CadastroQuestoes.java
+++ 8 Sem/hci/HCIGerardeProva/src/java/br/hci/geradorprova/cadastro/CadastroQuestoes.java
@@ -4,23 +4,52 @@
  */
 package br.hci.geradorprova.cadastro;
 
+import br.hci.geradorprova.utils.constantes.TipoQuestao;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author rudieri
  */
-public class CadastroQuestoes{
-    private ArrayList<String> materias = new ArrayList<String>();
-    private String selecionado;
+public class CadastroQuestoes implements Serializable {
+
+    private ArrayList<String> materias = new ArrayList<>();
+    private TipoQuestao tipoQuestao;
+    private String materiaSelecionada;
     
+    private HashMap<String, ArrayList<String>> assuntos = new HashMap<>();
+
     public CadastroQuestoes() {
         materias.add("Matemática");
         materias.add("Português");
+        materias.add("Física");
+        materias.add("Química");
+        materias.add("Geografia");
+        materias.add("História");
+        materias.add("Biologia");
+        materias.add("Filosofia");
+        adicionarAssunto("Matemática", "Expressões");
+        adicionarAssunto("Matemática", "Frações");
+        adicionarAssunto("Matemática", "Equação 1º Grau");
+        adicionarAssunto("Matemática", "Equação 2º Grau");
+        adicionarAssunto("Química", "Funções Orgânicas");
+        adicionarAssunto("Química", "Funções Inorgânicas");
+        adicionarAssunto("Filosofia", "O que se estuda nisso?");
+        adicionarAssunto("Filosofia", "WTF?");
+        
     }
 
-    
-    
+    private void adicionarAssunto(String materia, String assunto) {
+        ArrayList<String> listaAssuntosMateria = assuntos.get(materia);
+        if (listaAssuntosMateria == null) {
+            listaAssuntosMateria = new ArrayList<>();
+            assuntos.put(materia, listaAssuntosMateria);
+        }
+        listaAssuntosMateria.add(assunto);
+    }
+
     public ArrayList<String> getMaterias() {
         return materias;
     }
@@ -29,13 +58,36 @@ public class CadastroQuestoes{
         this.materias = materias;
     }
 
-    public String getSelecionado() {
-        return selecionado;
+    public ArrayList<String> getAssuntos() {
+        return assuntos.get(materiaSelecionada);
     }
 
-    public void setSelecionado(String selecionado) {
-        System.out.println(selecionado);
-        this.selecionado = selecionado;
+    public void setAssuntos(ArrayList<String> assuntos) {
+        this.assuntos.put(materiaSelecionada, assuntos);
+    }
+
+    public String getMateriaSelecionada() {
+        return materiaSelecionada;
+    }
+
+    public void setMateriaSelecionada(String materiaSelecionada) {
+        System.out.println("WTF? " + materiaSelecionada);
+        this.materiaSelecionada = materiaSelecionada;
+    }
+
+    public String getTipoQuestao() {
+        return tipoQuestao==null?"":tipoQuestao.getNome();
+    }
+
+    public void setTipoQuestao(String tipoQuestao) {
+        this.tipoQuestao = TipoQuestao.getPorNome(tipoQuestao);
+    }
+    public ArrayList<String> listarTipoQuestao(){
+        ArrayList<String> list= new ArrayList<>();
+        for (int i = 0; i < TipoQuestao.values().length; i++) {
+            list.add(TipoQuestao.values()[i].getNome());
+        }
+        return list;
     }
     
     
