cdd8af5ecb933a4ad39655418ce56c91353de16a
AraraRTC@gmail.com <AraraRTC@gmail.com@fe3ca8fe-d63a-39e7-fb67-3bc90b577958>
2012-09-03 22:21:31 +0000

    Adicionado Tipo de questão

--- trunk/8 Sem/hci/HCIGerardeProva/src/java/br/hci/geradorprova/cadastro/CadastroQuestoes.java
+++ trunk/8 Sem/hci/HCIGerardeProva/src/java/br/hci/geradorprova/cadastro/CadastroQuestoes.java
@@ -5,6 +5,7 @@
 package br.hci.geradorprova.cadastro;
 
 import br.hci.geradorprova.utils.constantes.TipoQuestao;
+import br.hci.geradorprova.utils.constantes.TipoQuestaoObjetiva;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,8 +18,8 @@ public class CadastroQuestoes implements Serializable {
 
     private ArrayList<String> materias = new ArrayList<>();
     private TipoQuestao tipoQuestao;
+    private TipoQuestaoObjetiva tipoQuestaoObjetiva;
     private String materiaSelecionada;
-    
     private HashMap<String, ArrayList<String>> assuntos = new HashMap<>();
 
     public CadastroQuestoes() {
@@ -38,7 +39,7 @@ public class CadastroQuestoes implements Serializable {
         adicionarAssunto("Química", "Funções Inorgânicas");
         adicionarAssunto("Filosofia", "O que se estuda nisso?");
         adicionarAssunto("Filosofia", "WTF?");
-        
+
     }
 
     private void adicionarAssunto(String materia, String assunto) {
@@ -76,19 +77,38 @@ public class CadastroQuestoes implements Serializable {
     }
 
     public String getTipoQuestao() {
-        return tipoQuestao==null?"":tipoQuestao.getNome();
+        return tipoQuestao == null ? "" : tipoQuestao.getNome();
     }
 
     public void setTipoQuestao(String tipoQuestao) {
         this.tipoQuestao = TipoQuestao.getPorNome(tipoQuestao);
     }
-    public ArrayList<String> listarTipoQuestao(){
-        ArrayList<String> list= new ArrayList<>();
+
+    public ArrayList<String> listarTipoQuestao() {
+        ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < TipoQuestao.values().length; i++) {
             list.add(TipoQuestao.values()[i].getNome());
         }
         return list;
     }
-    
-    
+
+    public String getTipoQuestaoObjetiva() {
+        return tipoQuestaoObjetiva == null ? "" : tipoQuestaoObjetiva.getNome();
+    }
+
+    public void setTipoQuestaoObjetiva(String tipoQuestaoObjetiva) {
+        this.tipoQuestaoObjetiva = TipoQuestaoObjetiva.getPorNome(tipoQuestaoObjetiva);
+    }
+
+    public ArrayList<String> listarTipoQuestaoObjetiva() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < TipoQuestaoObjetiva.values().length; i++) {
+            list.add(TipoQuestaoObjetiva.values()[i].getNome());
+        }
+        return list;
+    }
+
+    public boolean questoeEhObjetiva() {
+        return tipoQuestao != null && tipoQuestao == TipoQuestao.OBJETIVA;
+    }
 }
