3ae46803180317a67053641f28b80cc05134ffa4
AraraRTC <ararartc@gmail.com>
2012-09-03 22:21:31 +0000

    Adicionado Tipo de questão

--- 8 Sem/hci/HCIGerardeProva/src/java/br/hci/geradorprova/utils/constantes/TipoQuestaoObjetiva.java
+++ 8 Sem/hci/HCIGerardeProva/src/java/br/hci/geradorprova/utils/constantes/TipoQuestaoObjetiva.java
@@ -8,20 +8,28 @@ package br.hci.geradorprova.utils.constantes;
  *
  * @author rudieri
  */
-public enum TipoQuestao {
-    DESCRITIVA("Descritiva"),
-    OBJETIVA("Objetiva");
+public enum TipoQuestaoObjetiva {
+
+    MULTIPLA_ESCOLHA_3("Múltipla escolha (3 opções)"),
+    MULTIPLA_ESCOLHA_5("Múltipla escolha (5 opções)"),
+    VERDADEIRO_FALSO("Verdadeiro ou Falso"),
+    RELACIONAL("Relacional");
     private String nome;
 
-    private TipoQuestao(String nome) {
+    private TipoQuestaoObjetiva(String nome) {
         this.nome = nome;
     }
 
     public String getNome() {
         return nome;
     }
-    
-    public static TipoQuestao getPorNome(String nome){
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    public static TipoQuestaoObjetiva getPorNome(String nome) {
         for (int i = 0; i < values().length; i++) {
             if (values()[i].getNome().equals(nome)) {
                 return values()[i];
@@ -29,10 +37,4 @@ public enum TipoQuestao {
         }
         throw new IllegalArgumentException("Nome de constante inválido.");
     }
-
-    @Override
-    public String toString() {
-        return nome;
-    }
-    
 }
